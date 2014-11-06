069ee6e99e428fcff04bf7219be12d8fcd12a167
Danilo Margarido <bit2shift@gmail.com>
2014-11-06 05:36:57 +0000

    Replaced internal header with a dllmain source file Also removed the CGameEngine source file (now an header)

---
+++ src/implementation/dllmain.cpp
@@ -0,0 +1,24 @@
+#define R3V_EXPORT
+#include "../r3dVoxel.hpp"
+
+#define GLEW_MX
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <cstdio>
+#include <new>
+
+//////////////////////////////////////////
+//// MOST IMPORTANT PIECE OF OOP CODE ////
+//////////////////////////////////////////
+void r3dVoxel::IClass::Release()
+{
+	delete this;
+}
+
+//Singleton, of sorts
+r3dVoxel::IGameEngine* THE_GAME_ENGINE = 0;
+
+//Implementation headers
+#include "CMonitor.hpp"
+#include "CGameEngine.hpp"
--- src/internal/internal.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define GLEW_MX
-#define GLEW_STATIC
-#define R3V_EXPORT
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include "../r3dVoxel.hpp"
