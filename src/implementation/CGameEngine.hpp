51c6f597b0003811007e265a030b9b4b86ad289f
Danilo Margarido <bit2shift@gmail.com>
2014-11-06 05:44:23 +0000

    Implementation of IMonitor (CMonitor) and IGameEngine (CGameEngine) TODO CGameEngine::createView()

---
+++ src/implementation/CGameEngine.hpp
@@ -0,0 +1,85 @@
+/*
+ * GLFW callback used to notify about monitor events
+ */
+void r3vMonitorCallback(GLFWmonitor* monitor, int event)
+{
+	THE_GAME_ENGINE->getMonitors();
+}
+
+/*
+ * IGameEngine implementation
+ */
+class CGameEngine : public r3dVoxel::IGameEngine
+{
+	r3dVoxel::IMonitor** m_monitors;
+
+public:
+	CGameEngine() : m_monitors(0)
+	{
+		THE_GAME_ENGINE = this;
+		glfwSetMonitorCallback(r3vMonitorCallback);
+	}
+
+	~CGameEngine()
+	{
+		glfwSetMonitorCallback(0);
+		THE_GAME_ENGINE = 0;
+
+		//XXX game engine shutdown area
+
+		if(m_monitors)
+			delete[] m_monitors;
+
+		glfwTerminate();
+	}
+
+	///////////////////////////
+	//// Interface methods ////
+	///////////////////////////
+
+	r3dVoxel::IMonitor** getMonitors()
+	{
+		//get connected monitors
+		int count = 0;
+		GLFWmonitor** pmon = glfwGetMonitors(&count);
+
+		//deallocate old array, if existed
+		if(m_monitors)
+			delete[] m_monitors;
+
+		//allocate new array, with 1 extra slot, NULL if it fails (nothrow)
+		m_monitors = new (std::nothrow) r3dVoxel::IMonitor*[count + 1]();
+		if(m_monitors)
+		{
+			while(count--)
+				m_monitors[count] = new CMonitor(pmon[count]);
+		}
+
+		return m_monitors;
+	}
+
+	r3dVoxel::IMonitor* getPrimaryMonitor()
+	{
+		return new CMonitor(glfwGetPrimaryMonitor());
+	}
+
+	r3dVoxel::IView* createView()
+	{
+		//TODO finish
+		return 0;
+	}
+};
+
+R3VAPI r3dVoxel::IGameEngine* r3vInitialize()
+{
+	//Return NULL if engine already initialized
+	if(THE_GAME_ENGINE)
+		return THE_GAME_ENGINE;
+
+	//Initialize GLFW, return NULL if it fails
+	if(!glfwInit())
+		return 0;
+
+	//XXX hmm idk, but can have other stuff in here
+	return new CGameEngine();
+}
---
+++ src/implementation/CMonitor.hpp
@@ -0,0 +1,88 @@
+/*
+ * IMonitor implementation
+ */
+class CMonitor : public r3dVoxel::IMonitor
+{
+	GLFWmonitor* m_monitor;
+	r3dVoxel::SVideoMode* m_modes;
+
+private:
+	static void copyVideoMode(r3dVoxel::SVideoMode* vm, const GLFWvidmode* mode)
+	{
+		vm->valid   = 1;
+		vm->width   = mode->width;
+		vm->height  = mode->height;
+		vm->red     = mode->redBits;
+		vm->green   = mode->greenBits;
+		vm->blue    = mode->blueBits;
+		vm->refresh = mode->refreshRate;
+	}
+
+public:
+	CMonitor(GLFWmonitor* mon) : m_monitor(mon), m_modes(0) {}
+
+	~CMonitor()
+	{
+		if(m_modes)
+			delete[] m_modes;
+	}
+
+	///////////////////////////
+	//// Interface methods ////
+	///////////////////////////
+
+	/* CDT doesn't like the subscript on vector types */
+	r3dVoxel::math::ivec2 getPosition()
+	{
+		r3dVoxel::math::ivec2 pos;
+		glfwGetMonitorPos(m_monitor, &pos[0], &pos[1]);
+		return pos;
+	}
+
+	/* CDT doesn't like the subscript on vector types */
+	r3dVoxel::math::ivec2 getPhysicalSize()
+	{
+		r3dVoxel::math::ivec2 size;
+		glfwGetMonitorPhysicalSize(m_monitor, &size[0], &size[1]);
+		return size;
+	}
+
+	const char* getName()
+	{
+		return glfwGetMonitorName(m_monitor);
+	}
+
+	/*
+	 * GLFW takes care of freeing this struct,
+	 * so we just cast it to our type.
+	 */
+	const r3dVoxel::SVideoMode getVideoMode()
+	{
+		r3dVoxel::SVideoMode vm = {0};
+		const GLFWvidmode* mode = glfwGetVideoMode(m_monitor);
+		if(mode)
+			copyVideoMode(&vm, mode);
+		return vm;
+	}
+
+	const r3dVoxel::SVideoMode* getAllVideoModes()
+	{
+		//get available video modes
+		int count = 0;
+		GLFWvidmode* pvm = glfwGetVideoModes(m_monitor, &count);
+
+		//deallocate old array, if existed
+		if(m_modes)
+			delete[] m_modes;
+
+		//allocate new array, with 1 extra slot, NULL if it fails (nothrow)
+		m_modes = new (std::nothrow) r3dVoxel::SVideoMode[count + 1]();
+		if(m_modes)
+		{
+			while(count--)
+				copyVideoMode(&m_modes[count], &pvm[count]);
+		}
+
+		return m_modes;
+	}
+};
