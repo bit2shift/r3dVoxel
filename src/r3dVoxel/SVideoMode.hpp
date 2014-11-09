da299efe0ea39f31e8f430c8d49a6c80d4feee15
Danilo Margarido <bit2shift@gmail.com>
2014-11-09 19:09:18 +0000

    Small changes to SVideoMode to fit it inside a 64-bit integer Along with minor comment changes

--- src/r3dVoxel/SVideoMode.hpp
+++ src/r3dVoxel/SVideoMode.hpp
@@ -1,13 +1,12 @@
 /*
- * Defines a monitor's video mode
+ * Defines a monitor's video mode. Valid if refresh is not zero.
  * Example:
- *   SVideoMode vm = {1, 800, 600, 8, 8, 8, 59};
+ *   SVideoMode vm = {800, 600, 8, 8, 8, 59};
  */
 struct SVideoMode
 {
-	int valid;     //video mode valid if not 0
-	int width;     //screen width in pixels
-	int height;    //screen height in pixels
+	short width;     //screen width in pixels
+	short height;    //screen height in pixels
 	char red;      //red channel bit depth
 	char green;    //green channel bit depth
 	char blue;     //blue channel bit depth
