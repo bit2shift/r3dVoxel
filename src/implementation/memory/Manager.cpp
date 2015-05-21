76a04065ced8d77e1815f7a03009d635c43d02d6
Danilo Margarido <bit2shift@gmail.com>
2015-05-21 20:38:44 +0100

    Added atomicity to the memory subsystem

--- src/implementation/memory/Manager.cpp
+++ src/implementation/memory/Manager.cpp
@@ -1,10 +1,12 @@
 #include "Pool.hpp"
+#include <atomic>
 
 /*
  * Exporting the library functions from here,
  * so mirror the following define in here
  */
-#define R3VAPI    extern "C" [[gnu::dllexport]]
+#define R3VAPI         extern "C" [[gnu::dllexport]]
+#define THREAD_LOCK    volatile simple_mutex sm
 
 /*
  * This gets constructed upon library loading
@@ -12,18 +14,28 @@
  * Standard behaviour for DLL and SO
  */
 static Pool MEMORY_POOL;
+static std::atomic_flag MEMORY_LOCK = ATOMIC_FLAG_INIT;
+
+struct simple_mutex
+{
+	simple_mutex() { while(MEMORY_LOCK.test_and_set(std::memory_order_acquire)); }
+	~simple_mutex(){       MEMORY_LOCK.clear(std::memory_order_release);         }
+};
 
 R3VAPI void* r3vMalloc(std::size_t size)
 {
+	THREAD_LOCK;
 	return MEMORY_POOL.create(size);
 }
 
 R3VAPI void r3vFree(void* pointer)
 {
+	THREAD_LOCK;
 	MEMORY_POOL.destroy(pointer);
 }
 
 R3VAPI std::size_t r3vGetMemoryUsage()
 {
+	THREAD_LOCK;
 	return MEMORY_POOL.usage();
 }
