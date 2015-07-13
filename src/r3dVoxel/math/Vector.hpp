5e5ea7beb8bf256272fb02f90e578552beae1e2f
Danilo Margarido <bit2shift@gmail.com>
2015-07-13 03:19:46 +0100

    This was a pain in the back to fiddle. clang 3.6 doesn't like templated vector extensions.

--- src/r3dVoxel/math/Vector.hpp
+++ src/r3dVoxel/math/Vector.hpp
@@ -11,21 +11,20 @@ template<typename T>
 struct vec
 {
 	T x, y, z, w;
-
-	typedef T simd [[gnu::vector_size(16)]];
-	operator T*(){return reinterpret_cast<T*>(this);}
-	operator simd&(){return *reinterpret_cast<simd*>(this);}
 };
 
-/* predefined vector wrappers */
-typedef vec<float>         fvec_t;
-typedef vec<std::int32_t>  ivec_t;
-typedef vec<std::uint32_t> uvec_t;
+#define VEC(x) __attribute((vector_size(16))) x
 
 /* predefined vectors */
-typedef fvec_t::simd fvec;
-typedef ivec_t::simd ivec;
-typedef uvec_t::simd uvec;
+typedef VEC(float)         fvec;
+typedef VEC(std::int32_t)  ivec;
+typedef VEC(std::uint32_t) uvec;
+
+#ifdef __clang__
+#define shuffle(v,a,b,c,d) __builtin_shufflevector(v,v,a,b,c,d)
+#else
+#define shuffle(v,a,b,c,d) __builtin_shuffle(v,ivec{a,b,c,d})
+#endif
 
 fvec cross(fvec a, fvec b)
 {
@@ -34,14 +33,9 @@ fvec cross(fvec a, fvec b)
 	//cz = ax*by - ay*bx
 	//cw = aw*bw - aw*bw == 0
 
-	/* shuffle masks */
-	ivec m1 = {1, 2, 0, 3};
-	ivec m2 = {2, 0, 1, 3};
-
 	/* shuffle party */
-	fvec c1 = __builtin_shuffle(a, m1) * __builtin_shuffle(b, m2);
-	fvec c2 = __builtin_shuffle(a, m2) * __builtin_shuffle(b, m1);
-
+	fvec c1 = shuffle(a, 1, 2, 0, 3) * shuffle(b, 2, 0, 1, 3);
+	fvec c2 = shuffle(a, 2, 0, 1, 3) * shuffle(b, 1, 2, 0, 3);
 	return (c1 - c2);
 }
 
@@ -50,21 +44,17 @@ float dot(fvec a, fvec b)
 	//d = ax*bx + ay*by + az*bz + aw*bw
 	//aw == bw == 0
 
-	/* shuffle masks */
-	ivec m1 = {1, 0, 3, 2};
-	ivec m2 = {2, 3, 0, 1};
-
 	/* first step */
 	fvec c = (a * b);
 
 	/* horizontal addition, the shuffle way */
-	c += __builtin_shuffle(c, m1); // x+y     | y+x     | z+w     | w+z     |
-	c += __builtin_shuffle(c, m2); // x+y+z+w | y+x+w+z | z+w+x+y | w+z+y+x |
+	c += shuffle(c, 1, 0, 3, 2); // x+y     | y+x     | z+w     | w+z     |
+	c += shuffle(c, 2, 3, 0, 1); // x+y+z+w | y+x+w+z | z+w+x+y | w+z+y+x |
 	return c[0]; //c has the same value all over it
 }
 
 fvec norm(fvec v)
 {
-	/* I like built-in functions */
-	return (v / __builtin_sqrtf(dot(v, v)));
+	float d = sqrtf(dot(v, v));
+	return (v / fvec{d, d, d, d});
 }
