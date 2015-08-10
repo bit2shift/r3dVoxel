fdc3bdcb2a384fc7435719d6f5626ab0f3545ef3
Danilo Margarido <bit2shift@gmail.com>
2015-08-10 05:09:27 +0100

    Rewrite of non-throwing operator new
    Matches the standard library's behaviour regarding std::new_handler.
    Except the array variant calls its basic brother (not the throwing one).

--- inc/r3dVoxel/memops/new.hpp
+++ inc/r3dVoxel/memops/new.hpp
@@ -33,17 +33,28 @@ void* operator new[](std::size_t size)
 }
 
 /*
- * 3) Non-throwing non-array operator new.
+ * 3) Non-throwing non-array operator new. Called by (4).
  */
 void* operator new(std::size_t size, const std::nothrow_t&) noexcept
 {
-	return r3vMalloc(size);
+	while(true)
+	{
+		void* pointer = r3vMalloc(size);
+		if(pointer)
+			return pointer;
+
+		std::new_handler handler = std::get_new_handler();
+		if(handler)
+			handler();
+		else
+			return nullptr;
+	}
 }
 
 /*
- * 4) Non-throwing array operator new.
+ * 4) Non-throwing array operator new. Calls version (3).
  */
 void* operator new[](std::size_t size, const std::nothrow_t&) noexcept
 {
-	return r3vMalloc(size);
+	return ::operator new(size, std::nothrow);
 }
