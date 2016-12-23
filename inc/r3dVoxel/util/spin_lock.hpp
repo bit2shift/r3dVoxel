5e1ee591fb056364926e809a204c03bb07813d20
Danilo Margarido <bit2shift@gmail.com>
2016-12-23 19:21:30 +0000

    Whoops, try_lock would never work.

--- inc/r3dVoxel/util/spin_lock.hpp
+++ inc/r3dVoxel/util/spin_lock.hpp
@@ -16,9 +16,9 @@ namespace r3dVoxel
 			std::atomic_flag m_flag = ATOMIC_FLAG_INIT;
 
 		public:
-			void lock()     noexcept {  while(m_flag.test_and_set(std::memory_order_acquire)); }
-			void unlock()   noexcept {        m_flag.clear(std::memory_order_release);         }
-			bool try_lock() noexcept { return m_flag.test_and_set(std::memory_order_acquire);  }
+			void lock()     noexcept {   while(m_flag.test_and_set(std::memory_order_acquire)); }
+			void unlock()   noexcept {         m_flag.clear(std::memory_order_release);         }
+			bool try_lock() noexcept { return !m_flag.test_and_set(std::memory_order_acquire);  }
 		};
 	}
 }
