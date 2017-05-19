0137f5f606939d7f42a0829bff9842ce32498273
Danilo Margarido <bit2shift@gmail.com>
2017-05-19 05:40:09 +0100

    Add some constexpr-ness

--- inc/r3dVoxel/util/parameter_pack.hpp
+++ inc/r3dVoxel/util/parameter_pack.hpp
@@ -13,7 +13,7 @@ namespace r3dVoxel
 		struct parameter_pack
 		{
 			template<typename F, typename T, typename... P>
-			static void at(std::size_t index, F&& functor, T&& type, P&&... pack)
+			static constexpr void at(std::size_t index, F&& functor, T&& type, P&&... pack)
 			{
 				if(!index--)
 					std::forward<F>(functor)(std::forward<T>(type));
@@ -22,20 +22,20 @@ namespace r3dVoxel
 			}
 
 			template<typename F>
-			static void at(std::size_t, F&&){}
+			static constexpr void at(std::size_t, F&&){}
 
 			template<typename F, typename T, typename... P>
-			static void for_each(F&& functor, T&& type, P&&... pack)
+			static constexpr void for_each(F&& functor, T&& type, P&&... pack)
 			{
 				std::forward<F>(functor)(std::forward<T>(type));
 				for_each(std::forward<F>(functor), std::forward<P>(pack)...);
 			}
 
 			template<typename F>
-			static void for_each(F&&){}
+			static constexpr void for_each(F&&){}
 
 			template<typename T, typename... X, typename... Y>
-			static T list_cast(Y&&... y)
+			static constexpr T list_cast(Y&&... y)
 			{
 				return {X(std::forward<Y>(y))...};
 			}
