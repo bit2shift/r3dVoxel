2ad79577062d4db9370e70171da63dbe5db5712f
Danilo Margarido <bit2shift@gmail.com>
2018-07-12 11:42:59 +0100

    Whoops, PKG_CONFIG_PATH wasn't being set globally

--- dep/glfw3.mk
+++ dep/glfw3.mk
@@ -1,5 +1,6 @@
-all:: GLFW := $(shell git rev-parse --show-toplevel)/dep/glfw
-all:: PKG_CONFIG_PATH += $(GLFW)/lib/pkgconfig
+GLFW := $(shell git rev-parse --show-toplevel)/dep/glfw
+PKG_CONFIG_PATH += $(GLFW)/lib/pkgconfig
+
 all::
 	@printf "\033[36mBuilding GLFW\033[m\n"
 	@cmake -G"Unix Makefiles" -H$(GLFW) -B$(GLFW) -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=$(GLFW) -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF
